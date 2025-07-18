627a3d075424a525e434fb39a08e452057b01fd0
Brahim Beloucif <brahimbeloucif.dev@gmail.com>
2025-07-18 19:40:04 +0100

    Initial commit

---
+++ lib/screens/spalsh_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:model_app/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _ctrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _ctrl = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..forward();
+
+    _ctrl.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => HomeScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: SvgPicture.asset('assets/logo/logo.svg')),
+          Gap(10),
+          // هنا استبدلنا Container ب SizedBox + ClipRect
+          SizedBox(
+            width: 200,
+            child: Center(
+              child: ClipRect(
+                child: AnimatedBuilder(
+                  animation: _ctrl,
+                  builder: (ctx, child) => ShaderMask(
+                    shaderCallback: (bounds) {
+                      return LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [Colors.white, Colors.transparent],
+                        stops: [_ctrl.value, _ctrl.value],
+                      ).createShader(bounds);
+                    },
+                    blendMode: BlendMode.dstIn,
+                    child: child,
+                  ),
+                  child: SvgPicture.asset(
+                    'assets/logo/line.svg',
+                    width: 200,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
